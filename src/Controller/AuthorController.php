9b6d6a3d4da5c095488e96d2cf9784665a7cfc5a
Aymen Ftita <aymenftita1@gmail.com>
2023-10-01 22:57:21 +0200

    final push

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -44,10 +44,25 @@ class AuthorController extends AbstractController
             ]);
     }
 
-    #[Route('/auhtorDetails', name: 'details_twig')]
-    public function auhtorDetails($id){
-        return $this->render('author/showAuthor.html.twig', [
-            'id' => $id,
+    #[Route('/authorDetails/{id}', name: 'details_twig')]
+    public function auhtorDetails(int $id){
+
+        $authors = array(
+            array('id' => 1, 'picture' => '/images/Victor-Hugo.jpg','username' => ' Victor
+            Hugo', 'email' => 'victor.hugo@gmail.com ', 'nb_books' => 100),
+            array('id' => 2, 'picture' => '/images/william-shakespeare.jpg','username' => '
+            William Shakespeare', 'email' => ' william.shakespeare@gmail.com', 'nb_books' =>
+            200 ),
+            array('id' => 3, 'picture' => '/images/Taha_Hussein.jpg','username' => ' Taha
+            Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300),);
+
+            $author=null;
+            for ($i = 0; $i< count($authors); $i++){
+                if($authors[$i]['id']==$id){$author=$authors[$i];}
+            }
+
+            return $this->render('author/showAuthor.html.twig', [
+            'author' => $author,
         ]);
     }
 
